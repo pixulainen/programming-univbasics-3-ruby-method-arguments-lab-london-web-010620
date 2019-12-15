2b04b9a3f6e661a41d8211e656a21fd1ad9d8fdd
George Apetrei <george.apetrei@gmail.com>
2019-12-15 01:39:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction_with_language(name,language)
   return `Hi, my name is #{name} and I am learning to program in #{language}.`
 end
 
-def introduction_with_language(name,language)
+def introduction_with_language_optional(name,language="Ruby")
   return `Hi, my name is #{name} and I am learning to program in #{language}.`
 end
 
