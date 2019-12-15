d177f8a62334f7b81272c7b2b6a3b0c3d63ffc60
George Apetrei <george.apetrei@gmail.com>
2019-12-15 01:38:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
-
+def introduction(name)
+  return ``
+end
 
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
