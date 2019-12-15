26a00dde3e781f2ee8db312c21746d36dd404824
George Apetrei <george.apetrei@gmail.com>
2019-12-15 01:44:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,14 @@
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 def introduction(name)
-  return "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name,language)
-  return "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name,language="Ruby")
-  return "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
