5d2dcbd13770eeabfb26625e9d6ee9b76aae50ec
George Apetrei <george.apetrei@gmail.com>
2019-12-15 01:39:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,10 @@ def introduction(name)
   return `Hi, my name is #{name}.`
 end
 def introduction_with_language(name,language)
-  
+  return `Hi, my name is #{name} and I am learning to program in #{language}.`
 end
+
+
 
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
