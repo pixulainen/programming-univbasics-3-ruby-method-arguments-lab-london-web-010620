a7818a10b72da75a9b843d4a040efef970d3d438
George Apetrei <george.apetrei@gmail.com>
2019-12-15 01:38:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 def introduction(name)
-  return ``
+  return `Hi, my name is #{name}.`
 end
+
 
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
